2be1141a6c35083248bb4aa42f9db98136b24044
aTNa <atay.narynov@gmail.com>
2020-09-20 14:42:47 +0600

    Updates

---
+++ precache-manifest.75c5d7a4023c711a5b7fc6b728f8ea67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "557fca07412b4840a8a66ba4a90ef806",
+    "url": "/electionsmonitoringforms/index.html"
+  },
+  {
+    "revision": "d632a3b7a8e5e7ece708",
+    "url": "/electionsmonitoringforms/static/css/main.60779d42.chunk.css"
+  },
+  {
+    "revision": "7c935d092397ed79babb",
+    "url": "/electionsmonitoringforms/static/js/2.c6b5f1cd.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/electionsmonitoringforms/static/js/2.c6b5f1cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d632a3b7a8e5e7ece708",
+    "url": "/electionsmonitoringforms/static/js/main.a2a09d13.chunk.js"
+  },
+  {
+    "revision": "5afee54674311badeee6",
+    "url": "/electionsmonitoringforms/static/js/runtime-main.8b0b6ea8.js"
+  }
+]);

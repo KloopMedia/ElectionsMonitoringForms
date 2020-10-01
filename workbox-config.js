d9b6a03ab987e21aae31e5bb901135f9fc952d34
aTNa <atay.narynov@gmail.com>
2020-10-01 14:36:48 +0600

    Restore to before

--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,26 @@
 module.exports = {
-  "globDirectory": "src/",
+  "globDirectory": "build/",
   "globPatterns": [
-    "**/*.{css,js,json,svg}"
+    "**/*.{json,ico,html,png,js,txt,css}"
   ],
-  "swDest": "src/sw.js"
+  "swDest": "build/sw.js",
+  
+   // Define runtime caching rules.
+   runtimeCaching: [{
+    // Match any request that ends with .png, .jpg, .jpeg or .svg.
+    urlPattern: /\.(?:json)$/,
+
+    // Apply a cache-first strategy.
+    handler: 'CacheFirst',
+
+    options: {
+      // Use a custom cache name.
+      cacheName: 'forms',
+
+      // Only cache 10 images.
+      expiration: {
+        maxEntries: 10,
+      },
+    },
+  }],
 };
